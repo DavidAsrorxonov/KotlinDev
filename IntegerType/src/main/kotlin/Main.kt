3e6378afb08cfffb966583ee0af4532a830e2a12
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-08-20 15:42:23 +0500

    initial commit

---
+++ IntegerType/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val number = 10
+
+    val intMaxValue = Int.MAX_VALUE
+    val intMinValue = Int.MIN_VALUE
+
+    println("Hello, your age is $number")
+    println("Integer Max: $intMaxValue")
+    println("Integer Min: $intMinValue")
+
+    val byteMaxValue = Byte.MAX_VALUE
+    val byteMinValue = Byte.MIN_VALUE
+
+    println("Byte Max: $byteMaxValue")
+    println("Byte Min: $byteMinValue")
+
+    val shortMaxValue: Short = Short.MAX_VALUE
+    val shortMinValue: Short = Short.MIN_VALUE
+    println("Short Max: $shortMaxValue")
+    println("Short Min: $shortMinValue")
+
+    val longMaxValue: Long = Long.MAX_VALUE
+    val longMinValue: Long = Long.MIN_VALUE
+    println("Long Max: $longMaxValue")
+    println("Long Min: $longMinValue")
+}
---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var userName: String = "Dovud"
+    userName = "Tom"
+    val age: Int = 15
+    println("Hello $userName! His age is: $age")
+}
