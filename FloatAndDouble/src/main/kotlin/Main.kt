3f4fa1d3ee2323c92a9c5936b52e186848bab1e1
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-08-20 16:19:28 +0500

    play around with float and double

--- FloatAndDouble/src/main/kotlin/Main.kt
+++ FloatAndDouble/src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Float and Double")
+    val myNumber: Float = 2F
+    val myNumber2 = 2.0
+
+    val floatMax = Float.MAX_VALUE
+    val floatMin = Float.MIN_VALUE
+
+    val doubleMax = Double.MAX_VALUE
+    val doubleMin = Double.MIN_VALUE
+
+    println("myNumber: $myNumber")
+    println("myNumber: $myNumber2")
+
+    println("floatMax: $floatMax")
+    println("floatMin: $floatMin")
+
+    println("doubleMax: $doubleMax")
+    println("doubleMin: $doubleMin")
 }
